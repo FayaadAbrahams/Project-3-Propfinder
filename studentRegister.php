5d30c6313a24d054df40fd56b1fcf00550ce0c27
Fayaad Abrahams <fayaad1999@gmail.com>
2022-08-08 14:38:29 +0200

    PHP now correctly adds data - into database for PHPmyadmin.
    - Still need to create SQL file so that everyone can access database on their side.

--- studentRegister.php
+++ studentRegister.php
@@ -1,7 +1,7 @@
 <?php
-$txtId = $_POST['txtId'];
-$txtFirstName = $_POST['txtFirstName'];
-$txtLastName = $_POST['txtLastName'];
+
+$txtName = $_POST['txtName'];
+$txtPassword = $_POST['txtPassword'];
 $radGender = $_POST['radAnswer'];
 $txtEmail = $_POST['txtEmail'];
 $txtUni = $_POST['txtUni'];
@@ -18,9 +18,8 @@ if($conn->connect_error)
 {
     echo('Connection Failed: '.$conn->connect_error);
 }else{
-    $stmt = $conn->prepare("insert into studentregister(txtId, txtFirstName, txtLastName, radAnswer, txtEmail, txtUni, txtAddress, txtCell, birthday) values (?,?,?,?,?,?,?,?,?)");
-    $stmt->bind_param("issssssss",
-        $txtId,$txtFirstName,$txtLastName,$radGender,$txtEmail,$txtUni,$txtAddress,$txtCell,$dob);
+    $stmt = $conn->prepare("insert into student_register(fldName, fldPassword, fldGender, fldEmail, fldUni, fldAddress, fldCell, fldDOB) values (?,?,?,?,?,?,?,?)");
+    $stmt->bind_param("ssssssss",$txtName,$txtPassword,$radGender,$txtEmail,$txtUni,$txtAddress,$txtCell,$dob);
     $stmt->execute();
     echo "Registration Successful";
     $stmt->close();
